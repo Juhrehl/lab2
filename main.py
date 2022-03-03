43e01428e6f8a591fdfdae5a9b77a7d55c072e18
Jrl <100385287+Juhrehl@users.noreply.github.com>
2022-03-03 23:04:42 +0000

    lab 2 completed

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print ("hello")
+def part1():
+  n = 42 
+  tau = 3.14159 * 2
+  school = "Sault College"
+ 
+
+def part2():
+  class_name = "CSD110"        
+  desired_grade = 100     
+  hot2 = "hothot"
+  pi = 3.14159
+ 
+  
+def part3():
+  print ("Feeling"+"..."+("hot"*3)+"!")
+
+def part4():
+  base = 3000
+  height = 1000  
+  volume = 1/3 * int(base*base) * int(height)
+  print ("The volume of a square pyramid with base " + str(base) + "cm and height " +  str(height) + "cm is " + str(volume) + "cm^3")
+
+
+  
+part4()
